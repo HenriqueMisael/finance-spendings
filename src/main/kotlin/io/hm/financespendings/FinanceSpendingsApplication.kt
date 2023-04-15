67fea3cee707a224ddd60593a6ee5ec2bcb56a6a
Henrique Misael <henrique.nolear@gmail.com>
2023-04-15 14:44:14 -0300

    Initialize Spring Project
    Resolves: #1

---
+++ src/main/kotlin/io/hm/financespendings/FinanceSpendingsApplication.kt
@@ -0,0 +1,11 @@
+package io.hm.financespendings
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceSpendingsApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinanceSpendingsApplication>(*args)
+}
---
+++ src/test/kotlin/io/hm/financespendings/FinanceSpendingsApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.hm.financespendings
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinanceSpendingsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
